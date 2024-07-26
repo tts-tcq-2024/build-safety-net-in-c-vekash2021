cfa36f0395197e592993f91939fe2bbd85aa11fe
vekash2021 <106808639+vekash2021@users.noreply.github.com>
2024-07-26 21:18:11 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -5,69 +5,33 @@
 #include <ctype.h>
 #include <string.h>
 
-    static const char soundexTable[26] = {
-        '0', // A
-        '1', // B
-        '2', // C
-        '3', // D
-        '0', // E
-        '1', // F
-        '2', // G
-        '0', // H
-        '0', // I
-        '2', // J
-        '2', // K
-        '4', // L
-        '5', // M
-        '5', // N
-        '0', // O
-        '1', // P
-        '2', // Q
-        '6', // R
-        '2', // S
-        '3', // T
-        '0', // U
-        '1', // V
-        '0', // W
-        '2', // X
-        '0', // Y
-        '2'  // Z
-    };
-
-char getSoundexchar(char c) {
+char getSoundexCode(char c) {
+    const int soundexLUT[26] = {0, 1, 2, 3, 0, 1, 2, 0, 0, 2, 2, 4, 5, 5, 0, 1, 2, 6, 2, 3, 0, 1, 0, 2, 0, 2};
     c = toupper(c);
-if (c >= 'A' && c <= 'Z')
-   return soundexTable[c-'A'];
+    return '0'+soundexLUT[c-'A'];
 }
-
-void addtosoundex(char code, char *soundex,int sIndex)
-{
-        if (code != '0' && code != soundex[sIndex - 1]) {
-            soundex[sIndex++] = code;
-        }
-}
-
-int NameCharactersProcess(const char *name,char *soundex,int sIndex)
-{
-    int len = strlen(name);
-    soundex[0] = toupper(name[0]);
-    static char code;
-       for (int i = 1; i < len && sIndex < 4; i++) {
-        code = getSoundexchar(name[i]);
-		addtosoundex(code, soundex,sIndex);
+int updateSoundex(char code, int sIndex, char *soundex){
+    if (code != '0' && code != soundex[sIndex - 1]) {
+        soundex[sIndex++] = code;
     }
     return sIndex;
 }
-
-void generateSoundex(const char *name, char *soundex) {
-    static int sIndex = 1;
-    sIndex = NameCharactersProcess(name,soundex,sIndex);
- 
+void endSoundex(int sIndex, char *soundex){
     while (sIndex < 4) {
         soundex[sIndex++] = '0';
     }
-
     soundex[4] = '\0';
+}
+void generateSoundex(const char *name, char *soundex) {
+    int len = strlen(name);
+    soundex[0] = toupper(name[0]);
+    int sIndex = 1;
+
+    for (int i = 1; i < len && sIndex < 4; i++) {
+        char code = getSoundexCode(name[i]);
+        sIndex = updateSoundex(code, sIndex, soundex);
+    }
+    endSoundex(sIndex, soundex);
 }
 
 #endif // SOUNDEX_H
