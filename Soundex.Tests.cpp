7b53a17439210aa700decda9db58cbebacd217c8
vekash2021 <106808639+vekash2021@users.noreply.github.com>
2024-07-26 21:23:43 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -3,35 +3,31 @@
 
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
-  // char soundex[5];
-  // generateSoundex("AX", soundex);
+   char soundex[5];
+   generateSoundex("AX", soundex);
   // ASSERT_EQ(soundex,"A200");
-  // generateSoundex("Washington", soundex);
+  generateSoundex("Washington", soundex);
   // ASSERT_EQ(soundex,"W252");
-  // generateSoundex("Lee", soundex);
+   generateSoundex("Lee", soundex);
   // ASSERT_EQ(soundex,"L000");
-  // generateSoundex("Gutierrez", soundex);
+   generateSoundex("Gutierrez", soundex);
   // ASSERT_EQ(soundex,"G362");
-  // generateSoundex("Pfister", soundex);
+   generateSoundex("Pfister", soundex);
   // ASSERT_EQ(soundex,"P236");
-  // generateSoundex("Jackson", soundex);
+   generateSoundex("Jackson", soundex);
   // ASSERT_EQ(soundex,"J250");
-  // generateSoundex("Tymczak", soundex);
+   generateSoundex("Tymczak", soundex);
   // ASSERT_EQ(soundex,"T522");
-  // generateSoundex("VanDeusen", soundex);
+   generateSoundex("VanDeusen", soundex);
   // ASSERT_EQ(soundex,"V532");
-  // generateSoundex("Ashcraft", soundex);
+   generateSoundex("Ashcraft", soundex);
   // ASSERT_EQ(soundex,"A261");
-  // generateSoundex("Nguyen", soundex);
+  generateSoundex("Nguyen", soundex);
   // ASSERT_EQ(soundex,"N500");
-  // generateSoundex("Dube", soundex);
+   generateSoundex("Dube", soundex);
   // ASSERT_EQ(soundex,"D100");
-  // generateSoundex("", soundex);
+  generateSoundex("", soundex);
   // ASSERT_EQ(soundex,"0000");
-  char soundex[5];
-  generateSoundex("AX", soundex);
- // ASSERT_EQ(soundex,"A200");
-  generateSoundex("Tymczaak", soundex);
- // ASSERT_EQ(soundex,"T522");
+ 
 
 }
